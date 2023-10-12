7cd7643acf4a785ba327822c1d55975b0861ef94
rodolfowolff <rodolfoluizwolff@gmail.com>
2023-10-12 16:49:16 -0300

    feat: estado usuario fake renderizar diferente header

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -23,6 +23,11 @@ export default function NavbarComponent({
   routes: IRoutes[];
 }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [userAuth, setUserAuth] = useState({
+    username: "rwolff",
+    email: "a@a.com",
+    age: 25,
+  });
 
   return (
     <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
@@ -41,15 +46,30 @@ export default function NavbarComponent({
       </NavbarContent>
 
       <NavbarContent justify="end">
-        <NavbarItem className="hidden sm:flex">
-          <Link href="#" color="foreground">
-            Cadastro
-          </Link>
-        </NavbarItem>
-        <NavbarItem className="hidden sm:flex">
-          <Button as={Link} color="default" href="#" variant="flat">
-            Login
-          </Button>
+        <NavbarItem className="hidden sm:flex gap-2">
+          {userAuth && userAuth.username && (
+            <>
+              <Link href="#" color="foreground" size="sm">
+                Dashboard
+              </Link>
+
+              <Link
+                href="#"
+                color="foreground"
+                size="sm"
+                className="text-default-800 bg-slate-200 py-2 px-4 rounded"
+              >
+                Sair
+              </Link>
+            </>
+          )}
+
+          {!userAuth ||
+            (!userAuth.username && (
+              <Link href="#" color="foreground">
+                Cadastrar
+              </Link>
+            ))}
         </NavbarItem>
       </NavbarContent>
 
