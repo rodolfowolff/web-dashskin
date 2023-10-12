2b51665b380f9115f8845c68f4bd52d0e30e92c6
rodolfowolff <rodolfoluizwolff@gmail.com>
2023-10-12 17:10:40 -0300

    feat: loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Spinner } from "@nextui-org/react";
+
+export default function Loading() {
+  return (
+    <div className="w-full h-full">
+      <Spinner />
+    </div>
+  );
+}
