19818573dd72d67f01b929929c09cbcb67d9b68c
rodolfowolff <rodolfoluizwolff@gmail.com>
2023-10-12 16:45:28 -0300

    feat: icons delete, edit, visualizar

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,3 +2,6 @@ export * from './PlusIcon';
 export * from './VerticalDotsIcon';
 export * from './ChevronDownIcon';
 export * from './SearchIcon';
+export * from './DeleteIcon';
+export * from './EyeIcon';
+export * from './EditIcon';
